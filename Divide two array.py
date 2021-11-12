7cfe8895534cce67e57ad625113b3004a1b0a604
Arjun M <90825849+Arjun8420@users.noreply.github.com>
2021-11-12 18:03:05 +0530

    Add files via upload

---
+++ Divide two array.py
@@ -0,0 +1,9 @@
+#Division of two array using buildin functions
+import numpy as np
+
+a = np.array([[1,2],
+              [3,4]])
+b = np.array([[5,6],
+             [7,8]])
+
+print(np.divide(b,a))
